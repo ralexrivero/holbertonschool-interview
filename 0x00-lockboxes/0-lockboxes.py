fd7760e249d24fc7718db7902bf0f715391a44cb
ralexrivero <ralexrivero@gmail.com>
2022-04-25 02:47:14 +0000

    use the 0 key

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -11,6 +11,22 @@ def canUnlockAll(boxes):
     openBox = [False for i in range(len(boxes))]
 
     """
+    create a key chain to store keys retrieved from opened boxes
+    """
+    keyChain = [[0]]
+
+    """
+    got through key chain, open box if it is possible and
+    append new keys if not exists
+    and valids in the boxes range
+    """
+    for key in keyChain:
+        for k in key:
+            openBox[k] = True
+            
+    print(openBox)
+
+    """
     if any box is False (not open), return False
     otherwise return True
     """
