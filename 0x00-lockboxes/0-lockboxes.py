52b7497c8c1c52eb64e6bb615ef5248d2783f87b
ralexrivero <ralexrivero@gmail.com>
2022-04-25 02:35:02 +0000

    fix shebang and set an boolean for opened status

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,7 +1,13 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """ lockboxes """
-
+unlocked = [0]
 
 def canUnlockAll(boxes):
     """ can unlock all boxes? """
-    return
+    """
+    create array and populate with Booleans setted to false
+    to determine wich boxes are open
+    """
+    openBox = [False for i in range(len(boxes))]
+    print (openBox)
+    return True
