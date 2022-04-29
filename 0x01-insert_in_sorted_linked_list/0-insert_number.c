97943bc46919e15b97eb266c6a155cfd91bb8bb8
ralexrivero <ralexrivero@gmail.com>
2022-04-29 12:27:15 -0300

    prototype

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * insert_node - insert a number in sorted linked list
+ *
+ * @head: pointer to head of the list
+ * @number: number to insert
+ * Return: address of new node, NULL if it fail
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	return 0;
+}
