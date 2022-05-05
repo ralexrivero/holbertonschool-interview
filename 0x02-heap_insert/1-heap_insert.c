04c6780172c764b73f03bf05bb64f808830037bf
ralexrivero <ralexrivero@gmail.com>
2022-05-05 13:10:18 +0000

    base code

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -1,1 +1,13 @@
-heap_t *heap_insert(heap_t **root, int value);
+#include "binary_trees.h"
+/**
+ * heap_insert - Insert a value into a Max Binary Heap
+ *
+ * @root: double pointer to the root node of the Heap
+ * @value: value stored in node to be inserted
+ * Return: pointer to the inserted node, NULL on failure
+ */
+
+heap_t *heap_insert(heap_t **root, int value)
+{
+	return (NULL);
+}
--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -1,4 +1,8 @@
+#ifndef BINARY_TREES
+#define BINARY_TREES
 
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -10,11 +14,17 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
-
+typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s heap_t;
+
+/* prototypes */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+heap_t *heap_insert(heap_t **root, int value);
+
+#endif /* BINARY_TREES */
